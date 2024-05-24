6f04c066252f32d23113929532df69e623936567
Leonardo Gomes <leonardo.lopes555@gmail.com>
2024-05-24 01:15:13 -0300

    configuração firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,13 @@ import 'package:flutter/material.dart';
 import 'views/search.dart';
 import 'views/result.dart';
 import 'services/comunication_google_service.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main(){
+void main()async{
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
    runApp(
     MaterialApp(
       home: const SearchScreen(),
