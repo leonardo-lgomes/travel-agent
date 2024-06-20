95214ae35b80c6cf866eefc2dfb5a0cedb9418c6
Leonardo Gomes <leonardo.lopes555@gmail.com>
2024-06-19 22:00:52 -0300

    nova tela de resultados - front end

--- lib/views/result.dart
+++ lib/views/result.dart
@@ -9,6 +9,35 @@ class ResultScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     final data = ModalRoute.of(context)!.settings.arguments as Itinerary;
+
+    Container buildStyleDescriptionActivities(ActivitiesResultItinerary activities){
+      return Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          border: Border.all(color: Colors.grey, width: 1.0),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Text('${activities.time??""} - ${activities.name??""}'),
+              Text(activities.description??""),
+            ],
+          ),
+        )
+      );
+    }
+
+    Column buildActivitiesOfTheDay(List<ActivitiesResultItinerary> activities)
+    {
+      List<Widget> list = [];
+      for(var i = 0; i < activities.length; i++){
+          list.add(buildStyleDescriptionActivities(activities[i]));
+          list.add(Text("Categoria: " '${activities[i].type??""}'));
+      }
+      return Column(children: list);
+    }
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -24,29 +53,23 @@ class ResultScreen extends StatelessWidget{
               itemCount: listDays.length,
               itemBuilder: (context, index) {
                 DayResultItinerary day = listDays[index];
-                //List<ActivitiesResultItinerary>? listActivities = listDays[index].activities; 
-                return Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(day.title??""), 
-                    Text(day.description??""), 
-                    
-                    // ListView.builder(
-                    //   itemCount: listActivities!.length,
-                    //   itemBuilder: (context, index) {
-                    //     ActivitiesResultItinerary? activities = listActivities[index];
-                    //     return Column(
-                    //       crossAxisAlignment: CrossAxisAlignment.start,
-                    //       children: [
-                    //         Text(activities.name??""), 
-                    //         Text(activities.description??""), 
-                    //         Text(activities.time??""), 
-                    //         Text(activities.type??""), 
-                    //       ],
-                    //     );
-                    //   },
-                    // ),
-                  ],
+                return Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(day.title??"", style: const TextStyle(fontSize: 20.00, fontWeight:FontWeight.bold)), 
+                      Row(
+                        children: [
+                          const Icon(Icons.circle, size: 7.0), 
+                          const SizedBox(width: 8.0),
+                          Text(day.description??"")
+                        ],
+                      ),
+                      buildActivitiesOfTheDay(day.activities!),
+                    const Divider(endIndent: 1.0),
+                    ],
+                  ),
                 );
               },
             );
